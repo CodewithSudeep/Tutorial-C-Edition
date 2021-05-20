c52f0ecc9382e2276ec1215c6089307570dd2c44
Gita Khanal <thetechgirlgita@gmail.com>
2021-05-20 20:35:33 +0545

    Task7 and 8 in variable folder

---
+++ Week3/Variables/Task.7.c
@@ -0,0 +1,16 @@
+W3.7) What will be the output of the C program? Explain too.
+
+#include<stdio.h>
+int main()
+{
+	int class;
+	int public = 5;
+	int private = 10;
+	int protected = 15;
+	class = public + private + protected;
+	printf("%d",class);
+	return 0;
+}
+
+
+Ans.
---
+++ Week3/Variables/Task.8.c
@@ -0,0 +1,18 @@
+W3.8)  What will be the output of the C program? And explain the reason.
+#include<stdio.h>
+int main(){
+	int default = 5, a = 3;
+	if(a > 2)
+	printf("%d",default);	
+	return 0;
+}
+
+A. Compilation error
+
+B. Runtime error
+
+C. 5
+
+D. 3
+
+Ans.
