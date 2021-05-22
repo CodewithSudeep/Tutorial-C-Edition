b4f7f403f91a8653b6b138f4ca8e00163185200e
anupaaa <khatiwadaanupa506@gmail.com>
2021-05-22 21:28:41 +0545

    task.9

--- Week3/Constant and literals/Task3.c
+++
@@ -1,9 +0,0 @@
-W3.3) Write the example of code  to show few escape sequence characters.
-#include<stdio.h>
-#include<conio.h>
-int main()
-{
-	printf("hello\tworld\n");
-	return 0;
-}
-
---
+++ Week3/Constant and literals/task.9.c
@@ -0,0 +1,20 @@
+#include<conio.h>
+#include<conio.h>
+int main()
+{
+   int area,LENGTH,WIDTH,NEWLINE;  
+  
+   area = LENGTH * WIDTH;
+   printf("value of area : %d", area);
+   printf("%d",NEWLINE);
+
+   return 0;
+}
+
+
+
+
+
+output=00
+
+
