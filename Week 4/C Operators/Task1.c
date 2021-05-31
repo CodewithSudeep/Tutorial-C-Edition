02a4d24aa4d7b41b397b89bd501292e04de9e174
anupaaa <khatiwadaanupa506@gmail.com>
2021-05-31 21:27:50 +0545

    task1

--- Week 4/C Operators/Task1.c
+++ Week 4/C Operators/Task1.c
@@ -1,1 +1,12 @@
 W4.1) What is the modulus operator? Define it with example.
+
+->The modulus operator,written in most programming language as % or mod,perform what is
+known as the modulo operation.The modulus operator is a way to determine the
+ reminder of a division operation.Instead of returning the result of the division,the
+ modulo operation returns the whole number remainder.
+ 
+example:
+	A-B=-10
+	A*B=200
+	B/A=2
+	B%A=0
--- Week 4/C Operators/Task2.c
+++ Week 4/C Operators/Task2.c
@@ -1,1 +1,3 @@
 W4.2) Which of the following operators is incorrect and why? ( >=, <=, <>, ==)
+
+<> this operator is incorrect.
