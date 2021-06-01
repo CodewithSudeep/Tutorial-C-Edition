5360691929e42a64538ac6558e441334fa939cb5
Aayush450 <mishraayush8462@gmail.com>
2021-06-01 12:08:12 -0500

    some task complete

--- Week 4/Decision making in C/Task3.c
+++ Week 4/Decision making in C/Task3.c
@@ -1,1 +1,17 @@
-W4.3) Write a C program to find maximum between two numbers.
+//W4.3) Write a C program to find maximum between two numbers.
+#include <stdio.h>
+#include<conio.h>
+int main(){
+	int a,b,greater;
+	printf("Enter first numeber: ");
+	scanf("%d",&a);
+	
+	printf("enter second number:");
+	scanf("%d",&b);
+	greater=(a>b) ? a : b;
+	printf(" Greater value : %d", greater);
+	return 0;
+	
+	
+	
+}
