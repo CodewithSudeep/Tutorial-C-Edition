6a7d6dd8667fd182aec898bf0e3af193cd74cb04
anupaaa <khatiwadaanupa506@gmail.com>
2021-06-02 08:29:18 +0545

    task 6

--- Week 4/Decision making in C/Task6.c
+++ Week 4/Decision making in C/Task6.c
@@ -1,2 +1,13 @@
 W4.6) What are the decision making statements in C?
+->Decision making statements in C are as follows:
+	if statements
+	if else statements
+	nested if statements
+	if-else-if ladder
+	switch statements
+	jump statements
+	     -break
+	     -continue
+	     -goto
+	     -returns
 
