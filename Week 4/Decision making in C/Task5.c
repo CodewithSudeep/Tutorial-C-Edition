52154607513f3432973ef7716e2356448af1382c
anupaaa <khatiwadaanupa506@gmail.com>
2021-06-04 16:45:50 +0545

    task5

--- Week 4/Decision making in C/Task5.c
+++ Week 4/Decision making in C/Task5.c
@@ -1,2 +1,19 @@
-W4.5) Write a C program to check whether a year is leap year or not.
-Ans:
+// Write a C program to check whether a year is leap year or not.
+#include<stdio.h>
+#include<conio.h>
+int main();
+
+s{
+	int year;
+	printf("Enter a year: ");
+	scanf("%d",&year);
+	if(year % 400 ==0)
+	printf("%d is a leap year.",year); 
+	else if(year % 100 == 0)
+	printf("%d is not a leap year.",year);
+	else if(year % 4 == 0)
+	printf("%d is a leap year.",year);
+	else
+	printf("%d is not a leap year.",year);
+	return 0;
+}
