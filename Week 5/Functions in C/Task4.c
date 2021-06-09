348c9cabb150471effb6021d8ffe51ca33d9d9b4
anupaaa <khatiwadaanupa506@gmail.com>
2021-06-09 16:40:23 +0545

    task 4

--- Week 5/Functions in C/Task4.c
+++ Week 5/Functions in C/Task4.c
@@ -1,1 +1,12 @@
-W5.4)  Write a program to find the square of any number using the function.
+// Write a program to find the square of any number using the function.
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,square;
+	printf("Enter a number");
+	scanf("%d",&i);
+	square=i*i;
+	printf("the square is %d",i*i);
+	getch();
+}
