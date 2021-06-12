f1aa6f0f436f3bb0317a029be17abd008a3b903a
anupaaa <khatiwadaanupa506@gmail.com>
2021-06-12 21:50:47 +0545

    task 1 & task2

--- Week 6/Array/Task1.c
+++ Week 6/Array/Task1.c
@@ -1,5 +1,19 @@
-W6.1) Write a program in C to store ten elements in an array and print it. 
-- Input (1, 1, 2, ......, 10) 
-Expected output (1,1,2,3,4,5,6,7,8,9,10)
+// Write a program in C to store ten elements in an array and print it. 
+// Input (1, 1, 2, ......, 10) 
+//Expected output (1,1,2,3,4,5,6,7,8,9,10)
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i, j;
+	int arr[11] = {1,2,3,4,5,6,7,8,9,10};
+	printf("The elements of array are ");
+	for ( i=0; i<11; i++)
+	{
+		printf("%d",arr[i]);
+	}
 
-Ans: 
+	getch();
+}
+
+ 
--- Week 6/Array/Task2.c
+++ Week 6/Array/Task2.c
@@ -1,5 +1,21 @@
-W6.2)  Write a program in C to read n number of values in an array and display it in reverse order.
-- Input(2, 5, 7)
-- Expected output (7, 5, 2).
-
-Ans: 
+//2)  Write a program in C to read n number of values in an array and display 
+//it in reverse order.
+// Input(2, 5, 7)
+// Expected output (7, 5, 2).
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int arr[3]={2,5,7};
+	scanf("%d", &n);
+	for(i=0; i<n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	printf("\nArray in reverse order: ")
+	For(i=n-1; i>=0; i--)
+	{
+		printf("%d\t", arr[i]);
+	}
+	return0;
+}
