6b2929216b0db846ab50aae0d7be5200e6cf9750
thetechgirlgita <thetechgirlgita@gmail.com>
2021-06-23 10:52:46 +0545

    Mini project

---
+++ Week 8/Mini Project/Quiz-Game.c
@@ -0,0 +1,7 @@
+W8.4) Write a program in C for a simple Quiz game.
+
+(It should consists of simple MCQ based on sports, general knowledge, technical content etc. 
+Players just have to select the correct answer and can reach the next level.
+It is a very simple project with error-free sources.)
+
+--------------------------------------Best-Of-Luck-------------------------------------------
