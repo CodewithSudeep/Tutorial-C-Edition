f99e576cc4a464de550b405c262f1ceb0b6972e2
thetechgirlgita <thetechgirlgita@gmail.com>
2021-06-25 18:33:13 +0545

    chabges

--- Week 8/Mini Project/Quiz-Game.c
+++ Week 8/Mini Project/Quiz-Game.c
@@ -5,3 +5,5 @@ Players just have to select the correct answer and can reach the next level.
 It is a very simple project with error-free sources.)
 
 --------------------------------------Best-Of-Luck-------------------------------------------
+
+Ans: 
